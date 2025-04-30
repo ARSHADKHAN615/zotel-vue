1e9729b1eaf7ab590833466a7f686c9f967d1efc
Arshad Ali <webkhansite23@gmail.com>
2025-04-30 10:51:20 +0530

    refactor: simplify bookings table schema by removing unnecessary fields

--- database/migrations/2025_04_28_143140_create_bookings_table.php
+++ database/migrations/2025_04_28_143140_create_bookings_table.php
@@ -13,18 +13,10 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
-            $table->string('transaction_id')->unique();
             $table->foreignId('room_id')->constrained()->onDelete('cascade');
             $table->string('guest_name');
-            $table->string('customer_name');
-            $table->string('customer_email');
-            $table->enum('transaction_status', ['pending', 'confirmed', 'cancelled', 'completed'])->default('pending');
-            $table->string('room_type');
             $table->date('check_in');
             $table->date('check_out');
-            $table->decimal('base_amount', 10, 2);
-            $table->decimal('tax_amount', 10, 2);
-            $table->decimal('total_amount', 10, 2);
             $table->timestamps();
         });
     }
