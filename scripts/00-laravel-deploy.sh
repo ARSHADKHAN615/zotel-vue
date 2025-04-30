cbac37bfa19caf0e92f440f4b383fcf8f30d8707
Arshad Ali <webkhansite23@gmail.com>
2025-04-30 16:24:30 +0530

    chore: update Node.js installation commands in deployment script for consistency

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,8 +3,8 @@ echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Node install"
-npm install --prefix /var/www/html
-npm run build --prefix /var/www/html
+npm ci
+npm run build
 
 echo "Caching config..."
 php artisan config:cache
